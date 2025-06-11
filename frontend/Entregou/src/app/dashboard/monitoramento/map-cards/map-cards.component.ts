9bdc7d23bd2febe53e0bf408d0f55b6b5bfba88f
BruzacaF <fbruzacac@gmail.com>
2025-06-11 13:45:33 -0300

    feat: mock incial dos cards

--- frontend/Entregou/src/app/dashboard/monitoramento/map-cards/map-cards.component.ts
+++ frontend/Entregou/src/app/dashboard/monitoramento/map-cards/map-cards.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatGridTile } from '@angular/material/grid-list';
+import { StorageData, PackageData,ClientData, clients, packages, regionalStorages, localStorages } from '../../../../../public/resources/staticResources/fakeDataMap';
 
 
 @Component({
@@ -18,10 +19,28 @@ import { MatGridTile } from '@angular/material/grid-list';
 })
 export class MapCardsComponent {
 
-  
+  private storages: StorageData[] = [...regionalStorages, ...localStorages];
+  private packages: PackageData[] = [...packages];
+  private clients: ClientData[] = [...clients];
+  cardsData: any[] = [];
+
 
   constructor() {
+    this.cardsData = this.packageCardsData;
     
+  
   }
 
+  get packageCardsData() {
+    return this.packages.map((pkg) => {
+      return {
+        id: pkg.id,
+        client: clients.find(client => client.id === pkg.clientId)?.name || 'Cliente desconhecido',
+        status: pkg.status,
+        description: `Status: ${pkg.status}`,
+        destination: regionalStorages.find(storage => storage.id === pkg.destinationStorageId)?.name || localStorages.find(storage => storage.id === pkg.destinationStorageId)?.name || 'Destino desconhecido',
+        location: pkg.currentLocation ? `${pkg.currentLocation.lat}, ${pkg.currentLocation.long}` : 'Unknown Location'
+      };
+    });
+  }
 }
